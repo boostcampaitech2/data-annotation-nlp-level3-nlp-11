a148c34f7bf7f8bdaaa66e5e8cf2ea1ee28ba385
mycogno <jdg4661@gmail.com>
2021-11-16 01:41:59 +0000

    [fix] : amp(&) sign process (ex: AT&T)

--- mkdata.py
+++ mkdata.py
@@ -2,11 +2,11 @@ import json
 import os
 import pandas as pd
 
-with open('annotations-legend.json', "r") as j:
+with open("annotations-legend.json", "r") as j:
     annotation_info = json.load(j)
 
-path_html = './plain.html/pool/'
-path_json = './ann.json/master/pool'
+path_html = "./plain.html/pool/"
+path_json = "./ann.json/master/pool"
 
 dir_list = os.listdir(path_json)
 
@@ -14,62 +14,68 @@ jk = 0
 
 datafordf = []
 for folder in dir_list:
-    if folder[0] == '.':
+    if folder[0] == ".":
         continue
-    now_json = os.path.join(path_json,folder)
-    now_html = os.path.join(path_html,folder)
-    
+    now_json = os.path.join(path_json, folder)
+    now_html = os.path.join(path_html, folder)
+
     file_list = os.listdir(now_json)
     for file_name in file_list:
-        if file_name[0] == '.' or file_name[-4:] != 'json':
+        if file_name[0] == "." or file_name[-4:] != "json":
             continue
-        json_name = os.path.join(now_json,file_name)
-        html_name = os.path.join(now_html,'.'.join(file_name.split('.')[:-2]))+'.plain.html'
-        
-        with open(json_name,'r') as j:
+        json_name = os.path.join(now_json, file_name)
+        html_name = (
+            os.path.join(now_html, ".".join(file_name.split(".")[:-2])) + ".plain.html"
+        )
+
+        with open(json_name, "r") as j:
             data_info = json.load(j)
-        
-        
-        with open(html_name, 'r') as j:
+
+        with open(html_name, "r") as j:
             sentence = j.readlines()
-            sentence = ''.join(sentence).split('content')[2]
-            sentence = sentence.replace('&quot;','"')
-        
-            sentence = sentence.split('</pre>')[0]
-            sentence = sentence.split('>')[2]
-            sentence = sentence.strip('_')
-  
-        if len(data_info['entities']) != 2:
-            print('==============')
-            print('엔티티 수가 이상함')
+            sentence = "".join(sentence).split("content")[2]
+            sentence = sentence.replace("&quot;", '"')
+            sentence = sentence.replace("&amp;", "&")
+
+            sentence = sentence.split("</pre>")[0]
+            sentence = sentence.split(">")[2]
+            sentence = sentence.strip("_")
+
+        if len(data_info["entities"]) != 2:
+            print("==============")
+            print("엔티티 수가 이상함")
             print(sentence)
-            print('================')
+            print("================")
             continue
-        nowdic = {'sentence' : None, 'OBJ' : '', 'SUB' : '' } 
+        nowdic = {"sentence": None, "OBJ": "", "SUB": ""}
         before_start = -1
         for i in range(2):
-            info = data_info['entities'][i]
-            offset_info = info['offsets'][0]
-            start = offset_info['start']
+            info = data_info["entities"][i]
+            offset_info = info["offsets"][0]
+            start = offset_info["start"]
             if start > before_start:
-                start += i *7
+                start += i * 7
             before_start = start
-            end = start + len(offset_info['text'])
-            entity_type = annotation_info[info['classId']].split('_')
+            end = start + len(offset_info["text"])
+            entity_type = annotation_info[info["classId"]].split("_")
             nowdic[entity_type[0]] = entity_type[1]
-            if offset_info['text'] != sentence[start:end]:
-                print('=======코드 처리 필요: 이것들의 위치가 맞지 않음==========')
-                print(start,end,sentence[start:end],"|||||||",offset_info['text'])
+            if offset_info["text"] != sentence[start:end]:
+                print("=======코드 처리 필요: 이것들의 위치가 맞지 않음==========")
+                print(start, end, sentence[start:end], "|||||||", offset_info["text"])
                 print(sentence)
                 break
-            sentence = sentence[:start] + f'<{entity_type[0]} :{offset_info["text"]}>' + sentence[end:]
+            sentence = (
+                sentence[:start]
+                + f'<{entity_type[0]} :{offset_info["text"]}>'
+                + sentence[end:]
+            )
             if "___" in sentence:
-                sentence = sentence.split('___')[0]
-        nowdic['sentence'] = sentence
+                sentence = sentence.split("___")[0]
+        nowdic["sentence"] = sentence
         datafordf.append(nowdic)
 
-        
-df = pd.DataFrame(datafordf)
-df.to_csv('output.csv')
 
-print('Done')
+df = pd.DataFrame(datafordf)
+df.to_csv("output.csv")
+
+print("Done")
